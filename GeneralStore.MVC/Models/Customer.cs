1f85d37caae881c7cf63df1698114da313c6f6db
Jesse Smith <worldjesses@gmail.com>
2021-06-22 13:22:09 -0400

    Customer Class Created, Building out CRUD

---
+++ GeneralStore.MVC/Models/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GeneralStore.MVC.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int CustomerID { get; set; }
+        [Required]
+        [Display(Name = "FirstName")]
+        public string FirstName { get; set; }
+        [Required]
+        [Display(Name = "LastName")]
+        public string LastName { get; set; }
+        [Display(Name = "FullName")]
+        public string FullName { get; }
+    }
+}
